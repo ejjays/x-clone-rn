68e71b7778b070889d0af68249039a026f798bbb
ejjays <christsonalloso021@gmail.com>
2025-07-01 22:46:59 +0800

    fixing dependecy error

--- mobile/utils/constants.ts
+++ mobile/utils/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = "https://x-clone-rn-lilac.vercel.app/api"; 
+export const API_URL = "https://x-clone-rn-one.vercel.app/api"; 
 
 export const PUSHER_KEY = "ce15b36b8613a08a9f09";
 export const PUSHER_CLUSTER = "ap1"; 
