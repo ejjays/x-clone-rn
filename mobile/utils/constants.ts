714b74ef31ee93e9ec17358bb3777015c2c70a98
ejjays <christsonalloso021@gmail.com>
2025-07-01 21:59:12 +0800

    EXP MIGRATE TO PUSHER

--- mobile/utils/constants.ts
+++ mobile/utils/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = "YOUR_VERCEL_BACKEND_URL"; // Replace with your deployed backend URL
+export const API_URL = "https://x-clone-rn-lilac.vercel.app/api"; 
 
-export const PUSHER_KEY = "YOUR_PUSHER_KEY"; // Replace with your Pusher App Key
-export const PUSHER_CLUSTER = "YOUR_PUSHER_CLUSTER"; // Replace with your Pusher App Cluster
+export const PUSHER_KEY = "ce15b36b8613a08a9f09";
+export const PUSHER_CLUSTER = "ap1"; 
