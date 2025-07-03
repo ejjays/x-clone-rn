d4f78e3de0b81bff7faad0646ecdbfca241d2709
ejjays <christsonalloso021@gmail.com>
2025-07-03 16:56:48 +0800

    almost ...

--- mobile/components/NoNotificationsFound.tsx
+++ mobile/components/NoNotificationsFound.tsx
@@ -32,7 +32,7 @@ const NoNotificationsFound = () => {
     <View className="flex-1 items-center justify-center px-8 bg-white" style={{ minHeight: 400 }}>
       <View className="items-center">
         {/* BIG Animated Lottie Bell - Main Highlight */}
-        <View className="w-96 h-96 mb-8">
+        <View className="w-96 h-96 mt-2 mb-6">
           <LottieView
             ref={animationRef}
             source={require("../assets/animations/empty-notifications.json")}
