a33fae603115e86cade24da39ecc8a108f73c293
ejjays <christsonalloso021@gmail.com>
2025-07-07 13:01:42 +0800

    fixing error

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,6 +1,17 @@
+// mobile/babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@": "./",
+          },
+        },
+      ],
+    ],
   };
 };
