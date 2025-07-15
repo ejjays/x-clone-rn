46b8acd5f3e1ff06c9f4808ddfa499db257e08a7
ejjays <christsonalloso021@gmail.com>
2025-07-15 22:21:50 +0800

    ...

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,4 +1,4 @@
-import express from \"express\";
+import express from "express";
 import {
   followUnfollowUser,
   getCurrentUser,
@@ -6,19 +6,19 @@ import {
   syncUser,
   updateUserProfile,
   getAllUsers,
-} from \"../controllers/user.controller.js\";
-import { protectRoute } from \"../middleware/auth.middleware.js\";
+} from "../controllers/user.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 // Public routes
-router.get(\"/profile/:username\", getUserById);
+router.get("/profile/:username", getUserById);
 
 // Protected routes
-router.post(\"/sync\", protectRoute, syncUser);
-router.get(\"/me\", protectRoute, getCurrentUser);
-router.get(\"/all\", protectRoute, getAllUsers);
-router.put(\"/profile\", protectRoute, updateUserProfile);
-router.post(\"/follow/:userId\", protectRoute, followUnfollowUser);
+router.post("/sync", protectRoute, syncUser);
+router.get("/me", protectRoute, getCurrentUser);
+router.get("/all", protectRoute, getAllUsers);
+router.put("/profile", protectRoute, updateUserProfile);
+router.post("/follow/:userId", protectRoute, followUnfollowUser);
 
 export default router;
